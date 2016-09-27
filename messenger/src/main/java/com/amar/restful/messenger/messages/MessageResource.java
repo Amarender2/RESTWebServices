42e46d85bf8b64a568d965a42eff4f73775fec88
armekala <ammv8@mst.edu>
2016-09-27 13:15:50 -0500

    Returning JSON Response, Implementing POST Method. Add Header, Body in Postman API

--- messenger/src/main/java/com/amar/restful/messenger/messages/MessageResource.java
+++ messenger/src/main/java/com/amar/restful/messenger/messages/MessageResource.java
@@ -2,7 +2,9 @@ package com.amar.restful.messenger.messages;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,16 +19,23 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getMessages();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessageById(@PathParam ("messageId") long messageId) {
 		return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
 	}
 	
 }
