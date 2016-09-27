015def3d135c2c038b34db5bdc40fff33f8d4c67
armekala <ammv8@mst.edu>
2016-09-26 21:47:43 -0500

    Sample DB, Creating Service Stubs

---
+++ messenger/src/main/java/com/amar/restful/messenger/database/Database.java
@@ -0,0 +1,22 @@
+package com.amar.restful.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amar.restful.messenger.model.Message;
+import com.amar.restful.messenger.model.Profile;
+
+public class Database {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+	
+}
--- messenger/src/main/java/com/amar/restful/messenger/service/MessageService.java
+++ messenger/src/main/java/com/amar/restful/messenger/service/MessageService.java
@@ -2,15 +2,42 @@ package com.amar.restful.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import com.amar.restful.messenger.database.Database;
 import com.amar.restful.messenger.model.Message;
 
 public class MessageService {
-
+	private Map<Long, Message> messages = Database.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Message 1", "Amar" ));
+		messages.put(2L, new Message(2, "Message 2", "Amar" ));
+	}
+	
 	public List<Message> getMessages() {
-		List<Message> messages = new ArrayList<>();
-		messages.add(new Message(1, "Message 1", "Amar"));
-		messages.add(new Message(2, "Message 2", "Amar"));
-		return messages;
+		return new ArrayList<>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if(message.getId() < 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 	
 }
