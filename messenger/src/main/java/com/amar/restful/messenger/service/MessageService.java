7450a205418a0a9b01df88eae8da8cedcf5685d6
armekala <ammv8@mst.edu>
2016-09-28 14:25:45 -0500

    QueryParam, Pagination and Filtering

--- messenger/src/main/java/com/amar/restful/messenger/service/MessageService.java
+++ messenger/src/main/java/com/amar/restful/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.amar.restful.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +18,26 @@ public class MessageService {
 	
 	public List<Message> getMessages() {
 		return new ArrayList<>(messages.values());
+	}
+	
+	public List<Message> getMessagesByYear(int year) {
+		List<Message> messagesList = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values()) {
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year) {
+				messagesList.add(message);
+			}
+		}
+		return messagesList;
+	}
+	
+	public List<Message> getMessagesPaginated(int start, int size) {
+		List<Message> messagesList = new ArrayList<>(messages.values());
+		if(start + size > messagesList.size()) {
+			size = messagesList.size() - start;
+		}
+		return messagesList.subList(start, start + size);
 	}
 	
 	public Message getMessage(long id) {
